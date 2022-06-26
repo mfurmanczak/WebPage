37dd9fb703dd12963ddab65a58053c82fce85627
furmen42 <mfurmanczak@gmail.com>
2022-06-26 15:20:04 +0200

    first commit

---
+++ scripts/allscripts.js
@@ -0,0 +1,39 @@
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  // Check position of scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
+
+
+var navbar = document.getElementById("navbarWrapper");
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. 
+// Remove sticky when you leave the scroll position
+function addSticky() {
+  if (window.pageYOffset >= sticky - 150) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+document.addEventListener("scroll", addSticky);
